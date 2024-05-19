d4fce27b7930308cf6d1a74b54e41979a6c12888
Piotr Ruszkiewicz <pruszkie@c3r13s3.42warsaw.pl>
2024-05-19 13:38:46 +0200

    start

--- main.c
+++ main.c
@@ -1,47 +1,17 @@
 #include <mlx.h>
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+int main() {
+    void *mlx;
+    void *window;
+    void *img;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
+    mlx = mlx_init();
+    window = mlx_new_window(mlx, 800, 600, "Hello World");
+    mlx_loop(mlx);
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
+    img = mlx_new_image(mlx, 800, 600);
 
-int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 800, 600, "Hello world!");
-	img.img = mlx_new_image(mlx, 800, 600);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	my_mlx_pixel_put(&img, 6, 6, 0x00FF0000);
-	my_mlx_pixel_put(&img, 6, 7, 0x00FF0000);
-	my_mlx_pixel_put(&img, 7, 8, 0x00FF0000);
-	my_mlx_pixel_put(&img, 8, 10, 0x00FF0000);
-	my_mlx_pixel_put(&img, 9, 15, 0x00FF0000);
-	my_mlx_pixel_put(&img, 10, 20, 0x00FF0000);
-	my_mlx_pixel_put(&img, 11, 5, 0x00FF0000);
-	my_mlx_pixel_put(&img, 12, 5, 0x00FF0000);
-	my_mlx_pixel_put(&img, 13, 5, 0x00FF0000);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+    return 0;
 }
 
 // compile line: gcc *.c -I./minilibx-linux -L./minilibx-linux -lmlx_Linux -lXext -lX11 -lm
